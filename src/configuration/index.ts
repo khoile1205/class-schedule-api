4feab525cd709f7fdbfb1c4510e3f1506901d3b7
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2023-11-26 03:23:00 +0700

    append env to gitignore

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,6 +1,6 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    connectionString: `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER}/Develop`
+    connectionString: process.env.DATABASE_URL
   }
 });
